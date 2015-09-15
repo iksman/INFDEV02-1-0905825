571cb063a195056b82657fc53e773000037038bb
Iksman <w3ss3l12@gmail.com>
2015-09-15 14:41:26 +0200

    Asterix Program

---
+++ Asterix/Asterix/Asterix.py
@@ -0,0 +1,7 @@
+ended = 0
+start = 5
+while ended != 1:
+    print("*" * start);
+    start = start - 1
+    if start == 0:
+        ended = 1
--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,6 +1,8 @@
 str = input("What is your name? ");
 if str == "Python":
     print("What a coincidence, my name is also", str);
+elif str == "zzirGrizz":
+    print("My friend,",str,", it has been so long");
 else:
     print("Hello,", str);
 
