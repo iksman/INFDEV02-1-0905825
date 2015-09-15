61b00adaf7e4dc68921565804d6355f24c505bf6
Iksman <w3ss3l12@gmail.com>
2015-09-15 15:16:54 +0200

    Improved

--- Asterix/Asterix/Asterix.py
+++ Asterix/Asterix/Asterix.py
@@ -1,7 +1,10 @@
-ended = 0
-start = 5
-while ended != 1:
-    print("*" * start);
+﻿start = int(input("?>"));
+
+end = 0 
+startup = start + 2
+print("0" * startup);
+while start != -1:
+    print("0" + "*" * start + " " * end + "0")
     start = start - 1
-    if start == -1:
-        ended = 1
+    end = end + 1
+print("0" * startup);
