9b396c4942f67ab07552624ff74059321f4ae152
Iksman <wesselmjr@gmail.com>
2015-09-16 08:54:39 +0200

    Editted first command

--- Asterix/Asterix/Asterix.py
+++ Asterix/Asterix/Asterix.py
@@ -1,10 +1,13 @@
-﻿start = int(input("?>"));
+﻿try:    
+    start = int(input("Give me an integer> "));
+    end = 0 
+    startup = start + 2
+    print("0" * startup);
+    while start != -1:
+        print("0" + "*" * start + " " * end + "0")
+        start = start - 1
+        end = end + 1
+    print("0" * startup);
+except ValueError:
+    print("That's no integer!");
 
-end = 0 
-startup = start + 2
-print("0" * startup);
-while start != -1:
-    print("0" + "*" * start + " " * end + "0")
-    start = start - 1
-    end = end + 1
-print("0" * startup);
