662aca7bf23d162b96bfd15b8ddd52c5f419a0c4
Iksman <wesselmjr@gmail.com>
2015-10-05 19:41:41 +0200

    Added shapes and python 2.7 support to 5_1

--- 5_1/5_1/_5_1.py
+++ 5_1/5_1/_5_1.py
@@ -1,3 +1,11 @@
-userInput = input("Input>")
-userOutput = userInput[::-1]
-print(userOutput)
+﻿userInput = raw_input("Input>")
+length = len(userInput)
+counter = 0
+character = length - 1
+userOutput = ""
+while counter != length:
+    userOutput = userOutput + userInput[character]
+    character = character - 1
+    counter = counter + 1
+print userOutput
+
---
+++ 6_1/6_1/_6_1.py
@@ -0,0 +1,76 @@
+﻿import sys
+shape = raw_input("||1>A full square|| ||2>A hollow square|| ||3>A rectangle|| ||4>Isoceles||\nCommand>")
+if shape == "1" or shape == "2" or shape == "3" or shape == "4":
+    height = int(raw_input("height"))
+else:
+    height = 0
+if shape == "1" or shape == "2":
+    width = int(raw_input("width"))
+else:
+    width = 0
+
+coolwidth = width - 1
+coollength = height - 1
+lines = height + 1
+result = ""
+x = 0
+i = 0
+counter = 0
+done = False
+if shape == "1":
+    while counter != height:
+        for x in range(0, width):
+            result = result + "*"
+        result = result + "\n"
+        counter = counter + 1
+elif shape == "2":
+    while counter != lines:
+        if counter == 0:
+            for x in range(0, width):
+                result = result + "*"
+            result = result + "\n"
+            counter = counter + 1
+        elif counter == height:
+            for x in range(0, width):
+                result = result + "*"
+            result = result + "\n"
+            break
+        elif counter != coollength:
+            result = result + "*"
+            for x in range(1, coolwidth):
+                result = result + " "
+            result = result + "*"
+            result = result + "\n"
+            counter = counter + 1
+        else:
+            counter = height
+elif shape == "3":
+    counter = 1
+    while counter != lines:
+        for x in range(0,counter):
+            result = result + "*"
+        result = result + "\n"
+        counter = counter + 1
+elif shape == "4":
+    spaces = height - 1
+    chars = 1
+    while counter != height:
+        while done == False:
+            charpos = spaces + chars
+            secondpos = spaces + chars
+            for x in range(0, spaces):
+                result = result + " "
+            for x in range(spaces, charpos):
+                result = result + "*"
+            for x in range(charpos, secondpos):
+                result = result + " "
+            spaces = spaces - 1
+            chars = chars + 2
+
+            done = True
+        counter = counter + 1
+        result = result + "\n"
+        done = False
+else:
+    print "I don't know what",shape,"means!"        
+print result
