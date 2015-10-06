7f8162c42e068cd18da650dbcd85db91b8845d8b
Iksman <wesselmjr@gmail.com>
2015-10-06 15:14:09 +0200

    Fixed absolute zero and fixed for python 2.7

---
+++ 1_1/1_1/_1_1.py
@@ -0,0 +1,7 @@
+﻿while True:    
+    fahrenheit = float(raw_input("What is the temprature in Fahrenheit? "))
+    celcius = float((fahrenheit - 32.00) / 1.80 )
+    if celcius < -273.15:
+        print "can't go below absolute zero"
+    else:
+        print("%.2f" % celcius)
