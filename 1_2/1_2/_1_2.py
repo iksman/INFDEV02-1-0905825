842653f17574a37fd7a8356d386ea022fbf2ae02
Iksman <wesselmjr@gmail.com>
2015-10-06 15:11:19 +0200

    Python 2.7 Fixed

---
+++ 1_2/1_2/_1_2.py
@@ -0,0 +1,9 @@
+﻿while True :
+    celcius = float(raw_input("What is the temprature in Celcius? "))
+    if celcius < -273.14 :
+       print("Error: Cannot go below absolute zero")
+       
+    else:
+       fahrenheit = float((celcius + 32.00) * 1.80 )
+       print("%.2f" % fahrenheit)
+       break
---
+++ 1_3/1_3/_1_3.py
@@ -0,0 +1,7 @@
+﻿while True:
+    value = float(raw_input("Give me a number: "))
+    if value < 0 :
+        result = int(value) / int(-1)
+    else:
+        result = value
+    print("The absolute value of the number is", int(result))
